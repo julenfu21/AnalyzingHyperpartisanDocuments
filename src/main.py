bba0ec18f99140495ed0e2a794c10ad6156e24d0
Julen <jfuentes019@ikasle.ehu.eus>
2024-04-02 13:21:58 +0200

    Improve printed conted appearance

--- src/main.py
+++ src/main.py
@@ -1,32 +1,39 @@
-from pathlib import Path
-
-from src.get_hyperpartisan_data.HyperpartisanDocumentsLoader import HyperpartisanDocumentsLoader
+from src.constant_values.enums import DocumentType
 from src.get_hyperpartisan_data.HyperpartisanDocumentsProcessor import HyperpartisanDocumentsProcessor
 
+
 if __name__ == '__main__':
+    # Load hyperpartisan documents into txt files
     # hyperpartisan_documents_loader = HyperpartisanDocumentsLoader()
     # hyperpartisan_documents_loader.load_dataset()
 
+    # Process hyperpartisan documents
     hyperpartisan_documents_processor = HyperpartisanDocumentsProcessor(
-        hyperpartisan_documents_path=Path('../data/txt/hyperpartisan_short.txt'),
-        non_hyperpartisan_documents_path=Path('../data/txt/non-hyperpartisan_short.txt')
+        # hyperpartisan_documents_path=Path('../data/txt/hyperpartisan_short.txt'),
+        # non_hyperpartisan_documents_path=Path('../data/txt/non-hyperpartisan_short.txt')
     )
     hyperpartisan_document_list, non_hyperpartisan_document_list = (hyperpartisan_documents_processor.
                                                                     get_clean_documents())
 
-    print(f'Number of documents (Hyperpartisan): {len(hyperpartisan_document_list)}')
-    print(f'Number of documents (Non-Hyperpartisan): {len(non_hyperpartisan_document_list)}')
-    print(len(hyperpartisan_document_list) + len(non_hyperpartisan_document_list))
+    print(f'\n\tNumber of documents ({DocumentType.HYPERPARTISAN.value}): {len(hyperpartisan_document_list)}')
+    print(f'\tNumber of documents ({DocumentType.NON_HYPERPARTISAN.value}): {len(non_hyperpartisan_document_list)}')
+    print(f'\tTotal number of documents: {len(hyperpartisan_document_list) + len(non_hyperpartisan_document_list)}\n')
 
-    print(hyperpartisan_document_list[0])
-    print(non_hyperpartisan_document_list[0])
+    print(f'Document sample after performing preprocessing ({DocumentType.HYPERPARTISAN.value}):')
+    print(f'{hyperpartisan_document_list[0]}\n')
 
-    # Remove infrequent words (appear <20 times) before extracting log-odd ratios
+    print(f'\nDocument sample after performing preprocessing ({DocumentType.NON_HYPERPARTISAN.value}):')
+    print(f'{non_hyperpartisan_document_list[0]}\n')
+
+    # Remove infrequent words (they appear <20 times) before extracting log-odd ratios
     hyperpartisan_document_list, non_hyperpartisan_document_list = (hyperpartisan_documents_processor.
         remove_infrequent_words(
-            documents=(hyperpartisan_document_list, non_hyperpartisan_document_list),
-            threshold=3
+            documents=(hyperpartisan_document_list, non_hyperpartisan_document_list)
         )
     )
-    print(hyperpartisan_document_list[0])
-    print(non_hyperpartisan_document_list[0])
+
+    print(f'Document sample after removing infrequent words ({DocumentType.HYPERPARTISAN.value}):')
+    print(f'{hyperpartisan_document_list[0]}\n')
+
+    print(f'Document sample after removing infrequent words ({DocumentType.NON_HYPERPARTISAN.value}):')
+    print(f'{non_hyperpartisan_document_list[0]}\n')
